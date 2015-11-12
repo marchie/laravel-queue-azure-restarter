9cdd15a263d3f366d9e8b0445e13b39128d88800
Chris March <Chris.March@tfgm.com>
2015-11-12 15:16:25 +0000

    Config get

--- src/Console/PluginEnabledTrait.php
+++ src/Console/PluginEnabledTrait.php
@@ -1,16 +1,16 @@
 <?php
 namespace Marchie\LaravelQueueAzureRestarter\Console;
 
+use Illuminate\Support\Facades\Config;
+
 trait PluginEnabledTrait
 {
     protected function pluginEnabled()
     {
-        $config = $this->app['config'];
-
-        if (($config->get('laravel-queue-azure-restarter.kuduUser', env('KUDU_USER')) !== null)
-            && ($config->get('laravel-queue-azure-restarter.kuduPass', env('KUDU_PASS')) !== null)
-            && ($config->get('laravel-queue-azure-restarter.azureInstance', env('AZURE_INSTANCE')) !== null)
-            && ($config->get('laravel-queue-azure-restarter.queueFailTimeout', env('QUEUE_FAIL_TIMEOUT')) !== null))
+        if ((Config::get('laravel-queue-azure-restarter.kuduUser', env('KUDU_USER')) !== null)
+            && (Config::get('laravel-queue-azure-restarter.kuduPass', env('KUDU_PASS')) !== null)
+            && (Config::get('laravel-queue-azure-restarter.azureInstance', env('AZURE_INSTANCE')) !== null)
+            && (Config::get('laravel-queue-azure-restarter.queueFailTimeout', env('QUEUE_FAIL_TIMEOUT')) !== null))
         {
             return true;
         }
